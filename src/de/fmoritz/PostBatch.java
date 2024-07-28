33da967af3528077b45ff27f5f800a50b4cd30fb
Fynn Moritz <fynn-moritz@post.com>
2024-07-28 13:37:36 +0200

    refactor

--- src/de/fmoritz/PostBatch.java
+++ src/de/fmoritz/PostBatch.java
@@ -4,22 +4,31 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Executors;
 
+import de.fmoritz.interfaces.IPostService;
+import de.fmoritz.services.PostEingangsService;
+import de.fmoritz.services.PostServiceAsynchron;
+import de.fmoritz.services.SachbearbeiterService;
+import de.fmoritz.services.VersandService;
+import de.fmoritz.services.ZustaendikeitsService;
+
 public class PostBatch {
 
 	public static void main(String[] args) {
-		
+		final int MAX_THREAD_ANZAHL = 10000;
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 		String formattedTime = LocalTime.now().format(formatter);
 		System.out.println("Start: " + formattedTime);
 		
-		for (int i = 0; i < 10; i++) {
+//		IPostService postService = new PostService(new ZustaendikeitsService(), new SachbearbeiterService(), new VersandService(), new PostEingangsService());
+		IPostService postService = new PostServiceAsynchron(new ZustaendikeitsService(), new SachbearbeiterService(), new VersandService(), new PostEingangsService());
+		
+		for (int i = 0; i < 10; i++) { 
 			System.out.println("Lauf " + i);
 			long startTime = System.currentTimeMillis();
-			var postService = new PostService(new ZustaendikeitsService(), new SachbearbeiterService(), new VersandService());
 
-//			postService.verarbeite(() -> Executors.newFixedThreadPool(Integer.parseInt(args[0])));
 			postService.verarbeite(() -> Executors.newVirtualThreadPerTaskExecutor());
-//			postService.verarbeiteAsynchron(() -> Executors.newFixedThreadPool(Integer.parseInt(args[0])));
+			postService.verarbeite(() -> Executors.newFixedThreadPool(MAX_THREAD_ANZAHL));
+			postService.verarbeite(() -> Executors.newFixedThreadPool(Integer.parseInt(args[0])));
 
 			long endTime = System.currentTimeMillis();
 
